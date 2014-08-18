903919abf36fd320f5c514a0fd7c493ef36f9dda
xuwei-k <6b656e6a69@gmail.com>
2014-08-18 22:56:36 +0900

    InlineTest

---
+++ scalaz/src/test/scala/InlineTest.scala
@@ -0,0 +1,47 @@
+package zeroapply
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import scala.util.Try
+import scalaz._
+
+final class InlineTest {
+
+  private[this] val expectClassName = getClass().getName
+
+  private[this] def check(message: String, stackTrace: Array[StackTraceElement], index: Int, method: String) = {
+    val first = stackTrace.head
+    assertEquals(message, classOf[Thread].getName, first.getClassName)
+    assertEquals(message, "getStackTrace", first.getMethodName)
+
+    val actualStack = stackTrace(index)
+    assertEquals(message, expectClassName, actualStack.getClassName)
+    assertEquals(message, method, actualStack.getMethodName)
+  }
+
+  @Test
+  def test1(): Unit = {
+    val thread = Thread.currentThread
+    import thread.getStackTrace
+
+    val option = OptionApply.apply2(Option(1), Option("a"))((a, b) => getStackTrace).get
+    val either = EitherApply.apply2(Right(1), Right("a"))((a, b) => getStackTrace).right.get
+    val utilTry = TryApply.apply2(Try(1), Try("a"))((a, b) => getStackTrace).get
+    val disjunction = DisjunctionApply.apply2(\/-(1), \/-("a"))((a, b) => getStackTrace).getOrElse(null)
+    val maybe = MaybeApply.apply2(Maybe.just(1), Maybe.just("a"))((a, b) => getStackTrace).getOrElse(null)
+    val validationNel = ValidationNelApply.apply2(Success(1), Success("a"))((a, b) => getStackTrace).getOrElse(null)
+
+    (
+      option ::
+      either ::
+      utilTry ::
+      disjunction ::
+      maybe ::
+      validationNel ::
+      Nil
+    ).foreach{ stackTrace =>
+      check(stackTrace.take(10).mkString("\n"), stackTrace, 1, "test1")
+    }
+  }
+
+}
