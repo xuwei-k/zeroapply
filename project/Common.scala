380e3c8ef7a954ec73662462eb123baa8adcc416
xuwei-k <6b656e6a69@gmail.com>
2015-04-08 12:22:48 +0900

    avoid unused warnings in REPL

--- project/Common.scala
+++ project/Common.scala
@@ -73,6 +73,9 @@ object Common {
       "-language:existentials" ::
       "-language:higherKinds" ::
       "-language:implicitConversions" ::
+      Nil
+    ),
+    scalacOptions in compile ++= (
       "-Ywarn-unused" ::
       "-Ywarn-unused-import" ::
       Nil
