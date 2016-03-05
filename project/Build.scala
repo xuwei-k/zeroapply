e787c65bb21c36d1bfff9e747239f2ef8c9e525c
xuwei-k <6b656e6a69@gmail.com>
2016-03-06 00:49:46 +0900

    scalaz 7.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -45,7 +45,7 @@ object build extends Build {
       scalazVersion
     ),
     Common.generateSources := Boilerplate.scalaz(boilerplateMax),
-    scalazVersion := "7.2.0",
+    scalazVersion := "7.2.1",
     libraryDependencies ++= (
       ("org.scalaz" %% "scalaz-core" % scalazVersion.value) ::
       ("org.scalaz" %% "scalaz-scalacheck-binding" % scalazVersion.value % "test") ::
