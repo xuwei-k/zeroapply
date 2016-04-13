ef42b6eb94fc9e2ca35909d7d008620727e77b5c
xuwei-k <6b656e6a69@gmail.com>
2016-04-14 02:04:44 +0900

    Scala 2.12.0-M4

--- project/Common.scala
+++ project/Common.scala
@@ -87,7 +87,7 @@ object Common {
       Nil
     ) ::: unusedWarnings,
     scalaVersion := Scala211,
-    crossScalaVersions := Scala211 :: "2.12.0-M3" :: Nil,
+    crossScalaVersions := Scala211 :: "2.12.0-M4" :: Nil,
     scalacOptions in (Compile, doc) ++= {
       val tag = if(isSnapshot.value) gitHash else { "v" + version.value }
       Seq(
