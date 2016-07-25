6e868f71e3ced5bee8167d3f7dcdf33171c774d9
xuwei-k <6b656e6a69@gmail.com>
2016-07-25 09:49:16 +0900

    use build.sbt
    - https://github.com/sbt/sbt/commit/f2f12022a5672c3f57b8e5fce4227da455ab8024
    - https://github.com/sbt/sbt/blob/v0.13.12/main/src/main/scala/sbt/Build.scala#L11

---
+++ build.sbt
@@ -0,0 +1,65 @@
+import build._
+
+lazy val zeroapply = Project("zeroapply", file("zeroapply")).settings(
+  Common.baseSettings
+).enablePlugins(
+  BuildInfoPlugin
+).settings(
+  name := zeroapplyName,
+  buildInfoPackage := "zeroapply",
+  buildInfoObject := "BuildInfoZeroApply",
+  Common.generateSources := Boilerplate.zeroapply(boilerplateMax),
+  libraryDependencies ++= (
+    ("org.scala-lang" % "scala-reflect" % scalaVersion.value) ::
+    junit ::
+    Nil
+  )
+)
+
+lazy val scalaz = Project("scalaz", file("scalaz")).settings(
+  Common.baseSettings
+).enablePlugins(
+  BuildInfoPlugin
+).settings(
+  name := zeroapplyScalazName,
+  buildInfoPackage := "zeroapply",
+  buildInfoObject := "BuildInfoZeroApplyScalaz",
+  buildInfoKeys ++= Seq(
+    scalazVersion
+  ),
+  Common.generateSources := Boilerplate.scalaz(boilerplateMax),
+  scalazVersion := "7.2.4",
+  libraryDependencies ++= (
+    ("org.scalaz" %% "scalaz-core" % scalazVersion.value) ::
+    ("org.scalaz" %% "scalaz-scalacheck-binding" % scalazVersion.value % "test") ::
+    junit ::
+    Nil
+  )
+).dependsOn(zeroapply)
+
+lazy val root = Project("root", file(".")).settings(
+  Common.baseSettings ++ unidocSettings
+).settings(
+  name := "zeroapply-all",
+  Common.generateSources := Nil,
+  libraryDependencies ++= {
+    if(Sxr.enableSxr.value) {
+      Seq(
+        (libraryDependencies in scalaz).value,
+        (libraryDependencies in zeroapply).value
+      ).flatten
+    } else Nil
+  },
+  artifacts := Nil,
+  packagedArtifacts := Map.empty,
+  artifacts <++= Classpaths.artifactDefs(Seq(packageDoc in Compile)),
+  packagedArtifacts <++= Classpaths.packaged(Seq(packageDoc in Compile))
+).settings(
+  Sxr.settings1
+).settings(
+  Defaults.packageTaskSettings(
+    packageDoc in Compile, (UnidocKeys.unidoc in Compile).map{_.flatMap(Path.allSubpaths)}
+  )
+).settings(
+  Sxr.settings2
+).aggregate(zeroapply, scalaz)
--- project/Build.scala
+++ project/Build.scala
@@ -1,88 +1,18 @@
-import sbt._, Keys._
-import sbtbuildinfo.BuildInfoPlugin
-import sbtbuildinfo.BuildInfoPlugin.autoImport._
+import sbt._
 
-object build extends Build {
+object build {
 
-  private final val boilerplateMax = 22
+  final val boilerplateMax = 22
   val scalazVersion = SettingKey[String]("scalazVersion")
 
-  private val junit = "com.novocode" % "junit-interface" % "0.11" % "test"
+  val junit = "com.novocode" % "junit-interface" % "0.11" % "test"
 
-  private final val zeroapplyName = "zeroapply"
-  private final val zeroapplyScalazName = "zeroapply-scalaz"
+  final val zeroapplyName = "zeroapply"
+  final val zeroapplyScalazName = "zeroapply-scalaz"
 
   val modules: List[String] =
     zeroapplyName ::
     zeroapplyScalazName ::
     Nil
-
-  lazy val zeroapply = Project("zeroapply", file("zeroapply")).settings(
-    Common.baseSettings : _*
-  ).enablePlugins(
-    BuildInfoPlugin
-  ).settings(
-    name := zeroapplyName,
-    buildInfoPackage := "zeroapply",
-    buildInfoObject := "BuildInfoZeroApply",
-    Common.generateSources := Boilerplate.zeroapply(boilerplateMax),
-    libraryDependencies ++= (
-      ("org.scala-lang" % "scala-reflect" % scalaVersion.value) ::
-      junit ::
-      Nil
-    )
-  )
-
-  lazy val scalaz = Project("scalaz", file("scalaz")).settings(
-    Common.baseSettings : _*
-  ).enablePlugins(
-    BuildInfoPlugin
-  ).settings(
-    name := zeroapplyScalazName,
-    buildInfoPackage := "zeroapply",
-    buildInfoObject := "BuildInfoZeroApplyScalaz",
-    buildInfoKeys ++= Seq(
-      scalazVersion
-    ),
-    Common.generateSources := Boilerplate.scalaz(boilerplateMax),
-    scalazVersion := "7.2.4",
-    libraryDependencies ++= (
-      ("org.scalaz" %% "scalaz-core" % scalazVersion.value) ::
-      ("org.scalaz" %% "scalaz-scalacheck-binding" % scalazVersion.value % "test") ::
-      junit ::
-      Nil
-    )
-  ).dependsOn(zeroapply)
-
-  lazy val root = {
-    import sbtunidoc.Plugin._
-
-    Project("root", file(".")).settings(
-      Common.baseSettings ++ unidocSettings
-    ).settings(
-      name := "zeroapply-all",
-      Common.generateSources := Nil,
-      libraryDependencies ++= {
-        if(Sxr.enableSxr.value) {
-          Seq(
-            (libraryDependencies in scalaz).value,
-            (libraryDependencies in zeroapply).value
-          ).flatten
-        } else Nil
-      },
-      artifacts := Nil,
-      packagedArtifacts := Map.empty,
-      artifacts <++= Classpaths.artifactDefs(Seq(packageDoc in Compile)),
-      packagedArtifacts <++= Classpaths.packaged(Seq(packageDoc in Compile))
-    ).settings(
-      Sxr.settings1
-    ).settings(
-      Defaults.packageTaskSettings(
-        packageDoc in Compile, (UnidocKeys.unidoc in Compile).map{_.flatMap(Path.allSubpaths)}
-      )
-    ).settings(
-      Sxr.settings2
-    ).aggregate(zeroapply, scalaz)
-  }
 
 }
