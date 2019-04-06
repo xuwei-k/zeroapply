b250ab157213050db4fd1269b4763f3921c0f0c1
xuwei-k <6b656e6a69@gmail.com>
2019-04-06 20:23:24 +0900

    Scala 2.13.0-RC1

--- project/Common.scala
+++ project/Common.scala
@@ -89,7 +89,7 @@ object Common {
       }
       .toList,
     scalaVersion := Scala211,
-    crossScalaVersions := Scala211 :: "2.12.8" :: "2.13.0-M5" :: Nil,
+    crossScalaVersions := Scala211 :: "2.12.8" :: "2.13.0-RC1" :: Nil,
     scalacOptions in (Compile, doc) ++= {
       val tag =
         if (isSnapshot.value) gitHash
