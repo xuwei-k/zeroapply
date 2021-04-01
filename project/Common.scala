fa70e280faa33d27c16d94cd550f1f6c124c02bb
xuwei-k <6b656e6a69@gmail.com>
2021-04-01 09:41:27 +0900

    enable Scala 3 test

--- project/Common.scala
+++ project/Common.scala
@@ -89,7 +89,7 @@ object Common {
       }
       .toList,
     scalaVersion := Scala211,
-    crossScalaVersions := Scala211 :: "2.12.13" :: "2.13.5" :: Nil,
+    crossScalaVersions := Scala211 :: "2.12.13" :: "2.13.5" :: "3.0.0-RC2" :: Nil,
     (Compile / doc / scalacOptions) ++= {
       val tag =
         if (isSnapshot.value) gitHash
