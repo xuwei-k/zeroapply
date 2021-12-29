0c73fce3c7db717fdd551e76a1551d0bd31bb104
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-12-29 18:15:08 +0000

    Update junit-interface to 0.13.3

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ object build {
   final val boilerplateMax = 22
   val scalazVersion = SettingKey[String]("scalazVersion")
 
-  val junit = "com.github.sbt" % "junit-interface" % "0.13.2" % "test"
+  val junit = "com.github.sbt" % "junit-interface" % "0.13.3" % "test"
 
   final val zeroapplyName = "zeroapply"
   final val zeroapplyScalazName = "zeroapply-scalaz"
