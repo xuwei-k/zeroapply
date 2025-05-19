0e0c745e178efb6c8c0546ec0f4cab95466f3d51
xuwei-k <6b656e6a69@gmail.com>
2025-05-19 19:25:44 +0900

    fix scala-2/TryImpl

--- zeroapply/src/main/scala-2/TryImpl.scala
+++ zeroapply/src/main/scala-2/TryImpl.scala
@@ -5,15 +5,17 @@ import scala.reflect.macros.blackbox.Context
 final class TryImpl(override val c: Context) extends OptionBase {
   import c.universe._
 
-  override protected def getValDef(name: TermName, tpe: Type, body: Tree): Tree =
+  override protected def getValDef(name: TermName, tpe: Type, body: Tree): Tree = {
+    val e = TermName(c.freshName("e"))
     q"""
     val $name: _root_.scala.util.Try[$tpe] = try {
       $body
     } catch {
-      case _root_.scala.util.control.NonFatal(e) =>
-        _root_.scala.util.Failure(e)
+      case _root_.scala.util.control.NonFatal($e) =>
+        _root_.scala.util.Failure($e)
     }
     """
+  }
 
   override protected def getValue(value: Tree, tpe: Type): Tree =
     q"$value.get"
@@ -24,13 +26,15 @@ final class TryImpl(override val c: Context) extends OptionBase {
   /**
    * @see [[https://github.com/scala/scala/blob/v2.11.8/src/library/scala/util/Try.scala#L187-L194]]
    */
-  override protected def wrapSome(value: Tree): Tree =
+  override protected def wrapSome(value: Tree): Tree = {
+    val e = TermName(c.freshName("e"))
     q"""
     try _root_.scala.util.Success($value) catch {
-      case _root_.scala.util.control.NonFatal(e) =>
-        _root_.scala.util.Failure(e)
+      case _root_.scala.util.control.NonFatal($e) =>
+        _root_.scala.util.Failure($e)
     }
     """
+  }
 
   override protected def isEmpty(value: Tree): Tree =
     q"$value.isFailure"
