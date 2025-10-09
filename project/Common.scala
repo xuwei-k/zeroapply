a1816a4c23fca39ceb3aa830ff233529211d643b
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2025-10-09 09:27:32 +0000

    Update scala3-library to 3.3.7

--- project/Common.scala
+++ project/Common.scala
@@ -88,7 +88,7 @@ object Common {
       }
       .toList,
     scalaVersion := Scala212,
-    crossScalaVersions := Scala212 :: "2.13.17" :: "3.3.6" :: Nil,
+    crossScalaVersions := Scala212 :: "2.13.17" :: "3.3.7" :: Nil,
     (Compile / doc / scalacOptions) ++= {
       val tag =
         if (isSnapshot.value) gitHash
